9f0f7267a8d7d42894045d536e8bf576eb6c8374
James <K00213083@student.lit.ie>
2018-10-03 11:55:16 +0200

    Added fog effect and particles

---
+++ Assets/Player_Jumper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Jumper : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = other.
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Left");
+    }
+}
