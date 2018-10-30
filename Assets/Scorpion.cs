0d1a3c3b4a55ae6008194c060b36ac823607e302
James Fogarty <jfogarty86@gmail.com>
2018-10-30 09:24:06 +0000

    Walls Included
    DOwnloaded wall textures and animals

---
+++ Assets/Scorpion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorpion : MonoBehaviour {
+
+    public Transform Player;
+    int MoveSpeed = 1;
+    int MaxDist = 2;
+    int MinDist = 1;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Player);
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        //anim.SetTrigger("glide");
+        //anim.SetBool("isMoving", true);
+        
+        //anim.Play("Scorpion_walk");
+
+        //if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        //{
+
+        //    transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+        //    if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+        //    {
+        //        //Here Call any function U want Like Shoot at here or something
+        //    }
+
+        //}
+
+    }
+}
---
+++ Assets/SharkMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkMovement : MonoBehaviour
+{
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 3f;
+    private float characterVelocity = 2f;
+    private Vector3 movementDirection;
+    private Vector3 movementPerSecond;
+
+
+    void Start()
+    {
+        latestDirectionChangeTime = 0f;
+        calcuateNewMovementVector();
+    }
+
+    void calcuateNewMovementVector()
+    {
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        movementDirection = new Vector3(Random.Range(-1.0f, 1.0f), 0,Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = movementDirection * characterVelocity;
+    }
+
+    void Update()
+    {
+        //if the changeTime was reached, calculate a new movement vector
+        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        {
+            latestDirectionChangeTime = Time.time;
+            calcuateNewMovementVector();
+        }
+
+        //move enemy: 
+        transform.position = new Vector3(transform.position.x + (movementPerSecond.x * Time.deltaTime),
+        transform.position.z + (movementPerSecond.y * Time.deltaTime));
+
+    }
+}
